fef71a8ccf28e62a9d95beaca3d1ded4e0c7588a
Bunyamin Kilicer <bunyamin.kilicer@outlook.com>
2021-10-27 23:17:27 +0300

    First Commit

---
+++ FoursquareWithParse/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  FoursquareWithParse
+//
+//  Created by Bünyamin Kılıçer on 23.10.2021.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        /*
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
+        parseObject.saveInBackground { success, err in
+            
+            if err != nil{
+                print(err?.localizedDescription ?? "Error")
+            }else{
+                print("uploaded")
+            }
+        }
+         */
+        
+        let query = PFQuery(className: "Fruits")
+        query.findObjectsInBackground { objects, err in
+            
+            if err != nil {
+                print(err?.localizedDescription ?? "Error")
+            }
+            else{
+                print(objects)
+            }
+        }
+        
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FoursquareWithParse/Pods-FoursquareWithParse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoursquareWithParse : NSObject
+@end
+@implementation PodsDummy_Pods_FoursquareWithParse
+@end
